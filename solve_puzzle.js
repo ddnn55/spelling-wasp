2f7ef42887a88346a2ad5ae13db1d06a278f2bb7
David Stolarsky <david.stolarsky@gmail.com>
2021-07-14 15:47:23 -0700

    words by contained letter

--- solve_puzzle.js
+++ solve_puzzle.js
@@ -2,10 +2,22 @@ const fs = require('fs');
 const path = require('path');
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-const words = fs.readFileSync(path.join(__dirname, 'words.txt'), {encoding: 'utf-8'}).split('\n');
-console.log(words);
-
-for(let requiredChar of alphabet) {
-    
+const letters = new Set();
+const wordsByContainedLetter = {};
+for(let char of alphabet) {
+    letters.add(char);
+    wordsByContainedLetter[char] = [];
 }
+
+
+const words = fs.readFileSync(path.join(__dirname, 'words.txt'), {encoding: 'utf-8'}).split('\n').map(word => word.toLowerCase());
+
+for(let word of words) {
+    for(let char of word) {
+        if(letters.has(char)) {
+            wordsByContainedLetter[char].push(word);
+        }
+    }
+}
+
+console.log(wordsByContainedLetter)
