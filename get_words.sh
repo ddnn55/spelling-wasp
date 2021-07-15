65133df48f563c50099039ccc388fc5d90e49562
David Stolarsky <david.stolarsky@gmail.com>
2021-07-15 15:54:17 -0700

    also get 10k common words

--- get_words.sh
+++ get_words.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 curl "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt" > words.txt
+curl "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears.txt" > common_usa_words.txt
