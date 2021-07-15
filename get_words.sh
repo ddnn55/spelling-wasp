c22924db139b13d1ebea21c7f024e73275dfa78e
David Stolarsky <david.stolarsky@gmail.com>
2021-07-15 15:55:36 -0700

    doc

--- get_words.sh
+++ get_words.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# about half a million words. too many. many are not even words.
 # curl "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt" > words.txt
+
+# 10k most common words according to Google text corpus
 curl "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears.txt" > words.txt
